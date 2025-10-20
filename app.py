a2b7f07b20a1c6a00abc4ee175ced7f72dc62086
Munkiii8686 <soccerdamo@gmail.com>
2025-10-20 23:13:22 +1300

    Update app.py

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ def gen_ship(crew_roles, themes, traits, ship_types):
                 if member.split(":")[0] in crew_roles else 999)
     crew_html = ''.join(f"<li>{member}</li>" for member in crew)
     crew_count = len(crew)
-    ship = f"<h2>{ship_name}</h2><p><b>Ship Type:</b> {ship_type}</p><p><b>Ship Theme:</b> {theme}</p><p><b>Crew Count:</b> {crew_count}</p><ul>{crew_html}</ul>"
+    ship = f"<h1>{ship_name}</h1><p><b>Ship Type:</b> {ship_type}</p><p><b>Ship Theme:</b> {theme}</p><p><b>Crew Count:</b> {crew_count}</p><ul>{crew_html}</ul>"
     ship = f"<div class='ship-card'>{ship}</div>"
     return ship
 
@@ -215,13 +215,7 @@ def home():
                 max-width: 900px;
                 margin: auto;
             }}
-            title {{
-                font-family: Pirata One, serif;
-                color: #f0e9d2;
-                text-align: centre;
-                padding: 30px;
-            }}
-            h2 {{
+            h1, h2 {{
                 font-family: Pirata One, serif;
                 color: #000000;
                 text-align: centre;
@@ -263,7 +257,6 @@ def home():
             button:hover {{
                 background-color: #5bc0be;
             }}
-            h1, h2 {{ color: #f5f5f5; }}
         </style>
     </head>
     <body>
@@ -279,6 +272,7 @@ def home():
 # === START APP ===
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
